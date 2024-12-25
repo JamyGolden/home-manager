61f3faacab1e86cb3100cd3c541d19f6d052db12
Jamy Golden <code@jamygolden.com>
2024-12-25 11:41:21 +0100

    Add nvim descriptions for global keymaps

--- config/nvim/lua/jamygolden/config/keymaps.lua
+++ config/nvim/lua/jamygolden/config/keymaps.lua
@@ -9,10 +9,10 @@ local set_path_to_global_clipboard_func = function(is_absolute)
   end
 end
 
-map({ "n", "v" }, "<leader>y", '"+y', { silent = false })
-map({ "n" }, "<leader>Y", 'gg"+yG', { silent = false })
-map({ "n" }, "<C-u>", "<C-u>zz")
-map({ "n" }, "<C-d>", "<C-d>zz")
-map({ "v" }, "<leader>p", '"_dP')
-map({ "n" }, "<leader>cp", set_path_to_global_clipboard_func())
-map({ "n" }, "<leader>cP", set_path_to_global_clipboard_func(true))
+map({ "n", "v" }, "<leader>y", '"+y', { silent = false, desc = "Yank to global clipboard" })
+map({ "n" }, "<leader>Y", 'gg"+yG', { silent = false, desc = "Yank entire file to global cipboard" })
+map({ "n" }, "<C-u>", "<C-u>zz", { desc = "Centre content when navigating with <C-u>" })
+map({ "n" }, "<C-d>", "<C-d>zz", { desc = "Centre content when navigating with <C-d>" })
+map({ "v" }, "<leader>p", '"_dP', { desc = "Paste but don't change yanked content" })
+map({ "n" }, "<leader>cp", set_path_to_global_clipboard_func(), { desc = "Copy project-relative path to file" })
+map({ "n" }, "<leader>cP", set_path_to_global_clipboard_func(true), { desc = "Copy absolute path to file" })
